b2a781e9cfb789181fb6dc40329ccb5bfc526f87
Tatyana Koptashkina <tkoptashkina@croc.ru>
2025-02-23 17:13:42 +0300

    Добавлен allure для отчетов vol 2

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 from typing import List, Optional
 from datetime import datetime
 
+
 # создание экземпляра fast api, это уйдет в сваггер
 app = FastAPI(
     title="Diabetes Management API",
